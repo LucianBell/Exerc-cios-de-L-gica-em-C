f2b2953fd8b9bdfe5784525b1e41509bd3853a62
Lucian Bellini <lucianbellini1@gmail.com>
2023-05-18 20:09:54 -0300

    Adding first question to the game

--- computerFundamentalsGame/computerFundamentalsGame.c
+++ computerFundamentalsGame/computerFundamentalsGame.c
@@ -2,13 +2,37 @@
 
 int main()
 {
-    int points;
+    int points, anwser;
     char name[100];
+    points = 0;
 
-    printf("Hello! Let's play a game! First, what is your name?\n");
+    printf("***************************************************\n");
+    printf("*WELCOME TO THE COMPUTER SCIENCE FUNDAMENTALS GAME*\n");
+    printf("***************************************************\n");
+
+    printf("Hello! First things first, what is your name?\n");
     scanf("%99[^\n]", &name);
 
-    printf("Ooh, it is so nice to meet you %s!", name);
+    printf("Ooh, it is so nice to meet you %s!\n", name);
+    printf("To awnser the displayed question, just type the number of the anwser you selected\n");
 
+    printf("First question: What is the time complexity of a linear search algorithm?\n");
+    printf("1 - O(1)\n");
+    printf("2 - O(log n)\n");
+    printf("3 - O(n)\n");
+    scanf("%d", &anwser);
+
+    if (anwser < 1 || anwser > 3) {
+        printf("Invalid value! Enter a new anwser: ");
+        scanf("%d", &anwser);
+    } else if (anwser == 1 || anwser == 2) {
+        printf("Wrong anwser :/ Try again next time :D\n");
+    } else if (anwser == 3) {
+        printf("RIGHT ANWSER :D 10 points :D\n");
+        points = 10;
+    }
+
+    printf("Game over! Thanks for playing :D \n");
+    printf("Your score was %d, %s, congratulations!", points, name);
     return 0;
 }
